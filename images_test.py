407c16297c7b5bc4043df13e545511aed6095d91
hicksb <hicksb@berea.edu>
2022-06-22 14:27:31 -0400

    good shit

--- images_test.py
+++ images_test.py
@@ -22,3 +22,64 @@ label.pack()
 next_pic()
 
 win.mainloop()
+
+
+#################################################################################3
+from tkinter import *
+import time
+import random
+#
+# def move_up(event):
+#     label.place(x = label.winfo_x(), y=label.winfo_y()-10)
+# def move_down(event):
+#     label.place(x = label.winfo_x(), y=label.winfo_y()+10)
+# def move_left(event):
+#     label.place(x = label.winfo_x()-10, y=label.winfo_y())
+# def move_right(event):
+#     label.place(x = label.winfo_x()+10, y=label.winfo_y())
+#
+# window = Tk()
+# window.geometry("500x500")
+#
+# window.bind("<w>", move_up)
+# window.bind("<s>", move_down)
+# window.bind("<a>", move_left)
+# window.bind("<d>", move_right)
+# window.bind("<Up>", move_up)
+# window.bind("<Down>", move_down)
+# window.bind("<Left>", move_left)
+# window.bind("<Right>", move_right)
+#
+# images = ['This Ramen Is My Girlfriend.png']
+# for i in range(len(images)):
+#     myimage = PhotoImage(file=images[i])
+#     label = Label(window, image=myimage)
+#     label.place(x=0, y=0)
+#     time.sleep(5)
+#
+#
+# window.mainloop()
+
+def move_up(event):
+    canvas.move(myimage,0,-10)
+def move_down(event):
+    canvas.move(myimage,0,+10)
+def move_left(event):
+    canvas.move(myimage,-10,0)
+def move_right(event):
+    canvas.move(myimage,+10,0)
+
+window = Tk()
+
+window.bind("<w>",move_up)
+window.bind("<s>",move_down)
+window.bind("<a>",move_left)
+window.bind("<d>",move_right)
+
+canvas = Canvas(window,width=500,height=500)
+canvas.pack()
+
+photoimage = PhotoImage(file='This Ramen Is My Girlfriend.png')
+myimage = canvas.create_image(0,0,image=photoimage,anchor=NW)
+
+window.mainloop()
