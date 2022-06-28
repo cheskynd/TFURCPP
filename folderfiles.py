b6a156ab712dc29f43ec2eb11ea77b4a643433cc
landerosn <landerosn@berea.edu>
2022-06-28 10:12:30 -0400

    integration of resizing and transperancy in an image

--- folderfiles.py
+++ folderfiles.py
@@ -9,15 +9,15 @@ import tkinter
 from PIL import Image, ImageTk
 
 #shows transperancy with open CV
-# path = cv2.imread("blue.jpg")
-# bgra = cv2.cvtColor(path, cv2.COLOR_BGR2BGRA)
-# #transperancy number, if its 0 then it will be fully transparent
-# bgra[...,3] = 127
-# cv2.imwrite('result.png',bgra)
-# t= Image.open('result.png')
-# t.show()
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+path = cv2.imread("blue.jpg")
+bgra = cv2.cvtColor(path, cv2.COLOR_BGR2BGRA)
+#transperancy number, if its 0 then it will be fully transparent
+bgra[...,3] = 127
+cv2.imwrite('result.png',bgra)
+t= Image.open('result.png')
+t.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 #shows transperancy with tkinter
 
@@ -34,21 +34,21 @@ from PIL import Image, ImageTk
 
 
 #using cv to run a cycle of images with resizing
-# path = glob.glob("C:\Users\landerosn\Pictures\Marvel*.jpg")
-# images1 = []
-# for file in path:
-#     img= cv2.imread(file)
-#     img= cv2.resize(img, (200,200))
-#     images1.append(img)
-#     num_rows, num_cols = img.shape[:2]
-#     translate = np.float32([[1,0,70],[0,1,110]])
-#     imgtran = cv2.warpAffine(img, translate, (num_cols + 70, num_rows +110))
-#     translate = np.float32([[1, 0, -30], [0,1,-50]])
-#     imgtran = cv2.warpAffine(imgtran, translate, (num_cols + 70 + 30, num_rows +110 +50))
-#     cv2.namedWindow('translation', cv2.WINDOW_NORMAL)
-#     cv2.imshow("translation", imgtran)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+path = glob.glob("C:\Users\landerosn\Pictures\Marvel*.jpg")
+images1 = []
+for file in path:
+    img= cv2.imread(file)
+    img= cv2.resize(img, (200,200))
+    images1.append(img)
+    num_rows, num_cols = img.shape[:2]
+    translate = np.float32([[1,0,70],[0,1,110]])
+    imgtran = cv2.warpAffine(img, translate, (num_cols + 70, num_rows +110))
+    translate = np.float32([[1, 0, -30], [0,1,-50]])
+    imgtran = cv2.warpAffine(imgtran, translate, (num_cols + 70 + 30, num_rows +110 +50))
+    cv2.namedWindow('translation', cv2.WINDOW_NORMAL)
+    cv2.imshow("translation", imgtran)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 ##################################################################333
 #resizing image through TK
@@ -124,16 +124,21 @@ import os
 ##############################################################
 ## integration of resizing and transperancy
 
-root = tk.Tk()
-root.wm_attributes('-alpha', 0.3)
+# root = tk.Tk()
+# root.wm_attributes('-alpha', 0.3)
+#
+# image = Image.open("newyork.png")
+# resize_image = image.resize((400, 400))
+# img = ImageTk.PhotoImage(resize_image)
+# label1 = Label(image=img)
+# label1.image = img
+# label1.pack()
+# root.mainloop()
 
-image = Image.open("lion.png")
-resize_image = image.resize((400, 400))
-img = ImageTk.PhotoImage(resize_image)
-label1 = Label(image=img)
-label1.image = img
-label1.pack()
-root.mainloop()
+images = ['newyork.png', 'lion.png']
+for image in images:
+        img = Image.open(image)
+        img.show()
 
 
 
