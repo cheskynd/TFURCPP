7e3621c9cca302589c96f9ce86d4bd01106936a1
cheskynd <59712892+cheskynd@users.noreply.github.com>
2022-06-29 15:47:20 -0400

    Added todos in the Thenatofenestra.py file

--- Thenatofenestra.py
+++ Thenatofenestra.py
@@ -1,3 +1,4 @@
+# Supervisor/Professor: Dr. Jasmine Jones
 # Authors: Ndizeye Tschesquis, Blade Hicks, Nancy Landeros
 from pyfirmata import Arduino, util
 import time
@@ -5,30 +6,25 @@ import cv2
 import glob
 import numpy as np
 
-board = Arduino('COM4')
-path = glob.glob("C:\Users\landerosn\Pictures\Marvel*.jpg")
-images1 = []
+board = Arduino('/dev/cu.usbmodem1101')
 
 # Analog Pins
-Lsen = board.analog[1]
-Lsen1 = board.analog[3]
-temp = board.analog[2]
+temp = board.analog[1]  # temperature Sensor
+Lsen = board.analog[2]  # Light Sensor
+Lsen1 = board.analog[3]  # Light Sensor
 
 # Analog Reads
 tempRead = 0
-tempsens = 0
+tempSens = 0
 LsenRead = 0
 LsenRead1 = 0
-
-# Digital Pins
-LedR = board.digital[13]
-LedG = board.digital[12]
 
 # Analog setup
 it = util.Iterator(board)
 it.start()
 
 while True:
+
     Lsen.enable_reporting()
     temp.enable_reporting()
     Lsen1.enable_reporting()
@@ -36,48 +32,37 @@ while True:
     LsenRead1 = Lsen1.read()
     tempsens = temp.read()
 
-    ''' The inital readings are returned as NoneType so the following if statement
-    will set each reading to the average reading to avoid error'''
-    if type(LsenRead1) != float or type(LsenRead) != float or type(tempsens) != float:
-        LsenRead = 400
-        LsenRead1 = 400
-        tempsens = 0.1457
+    ''' The initial readings are returned as NoneType so the following if statements forces the program to 
+    run only when the reading no longer return NoneType values.'''
+    if type(LsenRead1) == float and type(LsenRead) == float and type(tempsens) == float:
 
-    '''The Values reading from the temperature and light sensors in Python are values between 0 and 1
-        so we multiply each read by 1000 to match the values retuned in the Arduino app.'''
-    LsenRead = LsenRead * 1000
-    LsenRead1 = LsenRead1 * 1000
-    tempRead = tempsens * 1000
+        '''The Values reading from the temperature and light sensors in Python are values between 0 and 1
+            so we scale by multiplying each read by 1000 to match the values returned in the Arduino app.'''
+        LsenRead = LsenRead * 1000
+        LsenRead1 = LsenRead1 * 1000
+        tempRead = tempsens * 1000
 
-    # The follows equations are used to calculate the temperature in Celcius
-    tempRead = tempRead / 1024
-    tempRead = tempRead * 5
-    tempRead = tempRead - 0.5
-    tempRead = tempRead * 100
+        # The following equations are used to calculate the temperature in Celcius. The initial readings are voltage
+        tempRead = tempRead / 1024
+        tempRead = tempRead * 5
+        tempRead = tempRead - 0.5
+        tempRead = tempRead * 100
 
-    print(tempRead)
-    if tempRead > 22:
-        # There is where a random picture is picked to be displayed once the temperature is greater then defined
-        if LsenRead > LsenRead1 and LsenRead > 500:
-            LedR.write(1)
-            LedG.write(0)
-        elif LsenRead < LsenRead1 and LsenRead1 > 500:
-            LedG.write(1)
-            LedR.write(0)
-        else:
-            LedG.write(0)
-            LedR.write(0)
+        if tempRead > 25:
 
-for file in path:
-    img = cv2.imread(file)
-    img = cv2.resize(img, (200, 200))
-    images1.append(img)
-    num_rows, num_cols = img.shape[:2]
-    translate = np.float32([[1, 0, 70], [0, 1, 110]])
-    imgtran = cv2.warpAffine(img, translate, (num_cols + 70, num_rows + 110))
-    translate = np.float32([[1, 0, -30], [0, 1, -50]])
-    imgtran = cv2.warpAffine(imgtran, translate, (num_cols + 70 + 30, num_rows + 110 + 50))
-    cv2.namedWindow('translation', cv2.WINDOW_NORMAL)
-    cv2.imshow("translation", imgtran)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+            # There is where a random picture is picked to be displayed once the temperature is greater then defined
+            if LsenRead - LsenRead1 >= 10 and LsenRead > 300:
+                # ToDo: Add code that will move the image up(down)
+
+                # ToDo: add another if statement for a higher LsenRead value to change the image
+                pass
+
+            elif LsenRead1 - LsenRead >= 10 and LsenRead1 > 300:
+                # ToDO: Add code that will move the image left(right)
+
+                # ToDo: add another if statement for a higher LsenRead1 value to change the image
+                pass
+
+            else:
+                # ToDo: add code that will return the image back to it initial location
+                print(str(tempRead) + '\n')
